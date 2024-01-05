a93bbb92bcfeca720aab392c9052732c3941bfec
Talia <talia.j.degisi@gmail.com>
2024-01-05 00:56:54 -0500

    Implement Twig on AoS Episode 1 page

--- twig-base.php
+++ twig-base.php
@@ -2,4 +2,5 @@
 require_once $_SERVER['DOCUMENT_ROOT'] . '/vendor/autoload.php';
 $loader = new \Twig\Loader\FilesystemLoader($_SERVER['DOCUMENT_ROOT'] . '/templates');
 $twig = new \Twig\Environment($loader);
+$twig->addExtension(new \Twig\Extension\PhpFunctionExtension());
 ?>
