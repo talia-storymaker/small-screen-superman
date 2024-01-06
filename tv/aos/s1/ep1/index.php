a93b6f89c9f05b294ec787b71d06f9931335be5a
Talia <talia.j.degisi@gmail.com>
2024-01-06 18:14:25 -0500

    Finish tv-episode template + misc stuff

--- tv/aos/s1/ep1/index.php
+++ tv/aos/s1/ep1/index.php
@@ -8,7 +8,19 @@ echo $twig->render('/tv-episode.html', [
     'alt' => 'Titlecard for Adventures of Superman: Superman on Earth'
   ),
   'content' => file_get_contents('index-contents.html'),
-  'hasGallery' => true,
-  'hasLinks' => true
+  'gallery' => array(
+    "scr002_Kryptonian_council_in_the_Temple_of_Wisdom.png",
+    "scr003_Jor-El_sitting_in_the_council.png",
+    "scr004_Ros-An_the_council_leader.png",
+    "scr010_Baby_Kal-El.png",
+    "scr016_Eben_Kent_retrieving_the_baby_Kal-El_from_the_burning_rocket.png",
+    "scr051_Clark's_first_appearance_as_Superman,_rescuing_the_man_dangling_on_a_rope_from_a_dirigible.png"
+  ),
+  'links' => array(
+    'IMDb Page' => 'https://www.imdb.com/title/tt0506593/',
+    '<i>The Adventures Continue</i> Review by Bruce Dettman' => 'http://www.jimnolt.com/SupermanOnEarthReview.htm',
+    '<i>The Adventures Continue</i> Comparative Review by Chris Brockow' => 'http://www.jimnolt.com//SupermanOnEarth.htm',
+    '<i>Superman Homepage</i> Review by James Lantz' => 'https://www.supermanhomepage.com/tv/tv.php?topic=reviews/aos-ep001'
+  )
 ]);
 ?>
