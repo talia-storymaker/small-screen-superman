fda5520b9933738a6bb69022f3bd071c3b10bbc0
Talia <talia.j.degisi@gmail.com>
2024-01-06 22:12:29 -0500

    Gallery template

--- tv/aos/s1/ep1/gallery.php
+++ tv/aos/s1/ep1/gallery.php
@@ -1,40 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-	<head>
-		<title>Adventures of Superman: Superman on Earth - Small Screen Superman</title>
-		<?php
-			include_once $_SERVER['DOCUMENT_ROOT'] . 'head.php';
-		?>
-	</head>
-	<body>
-		<?php
-			include_once $_SERVER['DOCUMENT_ROOT'] . 'header.php';
-		?>
-		<main>
-		<h1 id="beginning"><span class="seriesname">Adventures of Superman</span><br> Season 1 Episode 1: Superman on Earth</h1>
-		<h2 id="gallery">Gallery</h2>
-		<div class="gallery">
-			<p>There are currently <?php echo count(glob("media/screencaps/*.png")) ?> images for <a href="index.php">Superman on Earth</a>.</p>
-			<?php
-				$dirname = "media/screencaps/thumbs/";
-				$images = glob($dirname."*.png");
-
-				foreach($images as $image) {
-					echo '<img src="'.$image.'" class="gallery-item"> ';
-				}
-			?>
-		</div>
-		
-		<?php
-			include_once $_SERVER['DOCUMENT_ROOT'] . 'tv/gallerysegment.php'; 
-		?>
-		
-		</main>
-		
-		<?php
-			include_once $_SERVER['DOCUMENT_ROOT'] . 'footer.php';
-		?>
-		<script type="text/javascript" src="/js/add-alts-to-gallery.js"></script>
-		
-	</body>
-</html>		
+<?php
+require_once $_SERVER['DOCUMENT_ROOT'] . '/twig-base.php';
+echo $twig->render('/gallery.html', [
+  'pageTitle' => 'Adventures of Superman: Superman on Earth - Small Screen Superman', 
+  'contentTitle' => "<span class='seriesname'>Adventures of Superman</span><br /> Season 1 Episode 1: Superman on Earth",
+]);
+?>
--- tv/aos/s1/ep1/index.php
+++ tv/aos/s1/ep1/index.php
@@ -2,7 +2,7 @@
 require_once $_SERVER['DOCUMENT_ROOT'] . '/twig-base.php';
 echo $twig->render('/tv-episode.html', [
   'pageTitle' => 'Adventures of Superman: Superman on Earth - Small Screen Superman', 
-  'contentTitle' => "<span class='seriesname'>Adventures of Superman</span><br> Season 1 Episode 1: Superman on Earth",
+  'contentTitle' => "<span class='seriesname'>Adventures of Superman</span><br /> Season 1 Episode 1: Superman on Earth",
   'titleCard' => array(
     'src' => 'media/screencaps/scr001_Titlecard.png',
     'alt' => 'Titlecard for Adventures of Superman: Superman on Earth'
