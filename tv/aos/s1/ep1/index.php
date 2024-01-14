a56f836825fa8ea6bdca85858817fae3a1560c75
Talia <talia.j.degisi@gmail.com>
2024-01-14 00:24:25 -0500

    Convert AOS s1ep1 images to jpg

--- tv/aos/s1/ep1/index.php
+++ tv/aos/s1/ep1/index.php
@@ -4,17 +4,17 @@ echo $twig->render('/tv-episode.html', [
   'pageTitle' => 'Adventures of Superman: Superman on Earth - Small Screen Superman', 
   'contentTitle' => "<span class='seriesname'>Adventures of Superman</span><br /> Season 1 Episode 1: Superman on Earth",
   'titleCard' => array(
-    'src' => 'media/screencaps/scr001_Titlecard.png',
+    'src' => 'media/screencaps/scr001_Titlecard.jpg',
     'alt' => 'Titlecard for Adventures of Superman: Superman on Earth'
   ),
   'content' => file_get_contents('index-contents.html'),
   'gallery' => array(
-    "scr002_Kryptonian_council_in_the_Temple_of_Wisdom.png",
-    "scr003_Jor-El_sitting_in_the_council.png",
-    "scr004_Ros-An_the_council_leader.png",
-    "scr010_Baby_Kal-El.png",
-    "scr016_Eben_Kent_retrieving_the_baby_Kal-El_from_the_burning_rocket.png",
-    "scr051_Clark's_first_appearance_as_Superman,_rescuing_the_man_dangling_on_a_rope_from_a_dirigible.png"
+    "scr002_Kryptonian_council_in_the_Temple_of_Wisdom.jpg",
+    "scr003_Jor-El_sitting_in_the_council.jpg",
+    "scr004_Ros-An_the_council_leader.jpg",
+    "scr010_Baby_Kal-El.jpg",
+    "scr016_Eben_Kent_retrieving_the_baby_Kal-El_from_the_burning_rocket.jpg",
+    "scr051_Clark's_first_appearance_as_Superman,_rescuing_the_man_dangling_on_a_rope_from_a_dirigible.jpg"
   ),
   'links' => array(
     'IMDb Page' => 'https://www.imdb.com/title/tt0506593/',
