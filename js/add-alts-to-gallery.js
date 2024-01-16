bb215cee954b4b67edcb78c4feccab212baf9514
Talia <talia.j.degisi@gmail.com>
2024-01-15 23:37:24 -0500

    JPG-ize and name images (AOS s1 ep3 and AOS s1 ep7) (Previous commits didn't work)

--- js/add-alts-to-gallery.js
+++ js/add-alts-to-gallery.js
@@ -1,4 +1,4 @@
 var galleryImages = document.getElementsByClassName('gallery-item');
 for (var i = 0; i < galleryImages.length; i++) {
-	galleryImages[i].alt = galleryImages[i].src.split("/").pop().slice(7, -4).replace(/_/g, ' ');
+	galleryImages[i].alt = galleryImages[i].src.split("/").pop().slice(7, -4).replace(/_/g, ' ').replace("COLON", ":");
 }
--- tv/aos/s1/ep7/index.php
+++ tv/aos/s1/ep7/index.php
@@ -4,10 +4,15 @@ echo $twig->render('/tv-episode.html', [
   'pageTitle' => 'Adventures of Superman: The Birthday Letter - Small Screen Superman', 
   'contentTitle' => "<span class='seriesname'>Adventures of Superman</span><br /> Season 1 Episode 7: The Birthday Letter",
   'titleCard' => array(
-    'src' => 'media/screencaps/scr001_Titlecard.png',
+    'src' => 'media/screencaps/scr001_Titlecard.jpg',
     'alt' => 'Titlecard for Adventures of Superman: The Birthday Letter'
   ),
   'content' => file_get_contents('index-contents.html'),
+  'gallery' => array(
+    "scr002_Lois_Lane_reads_the_letter_from_Kathy_to_Superman_aloud_to_Perry_White.jpg",
+    "scr009_Virginia_stands_by_while_Superman_reads_Kathy's_note.jpg",
+    "scr033_Slugger_and_Kathy_smiling_at_each_other.jpg"
+  ),
   'links' => array(
     'IMDb Page' => 'http://www.imdb.com/title/tt0506600/',
     '<i>The Adventures Continue</i> Review by Bruce Dettman' => 'http://www.jimnolt.com/BirthdayLetterReview.htm',
