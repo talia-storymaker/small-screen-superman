26bd03b692f2f9e22354206197c16c5c8da1bc94
Talia <talia.j.degisi@gmail.com>
2024-02-08 00:44:50 -0500

    Add images to AOS s1 ep4 synopsis

--- tv/aos/s1/ep4/index.php
+++ tv/aos/s1/ep4/index.php
@@ -11,8 +11,7 @@ echo $twig->render('/tv-episode.html', [
     "scr007_Lois_Lane_in_the_artcraft_shop,_confused_and_a_bit_startled.jpg",
     "scr012_Clark_sits_on_Lois'_desk_with_a_smile_and_talks_with_her.jpg",
     "scr022_Lois_excitedly_talks_on_the_phone.jpg",
-    "scr045_Martin_attempts_to_interrogate_Lois_while_Dorn_stands_menacingly_in_the_background.jpg",
-    "scr058_Clark_shows_Lois_and_the_Inspector_a_chalkboard_with_pictures_and_names_of_the_items_from_the_statues.jpg"
+    "scr045_Martin_attempts_to_interrogate_Lois_while_Dorn_stands_menacingly_in_the_background.jpg"
   ),
   'imdbId' => 'tt0506579',
   'tacId' => 'BrokenStatuesReview',
