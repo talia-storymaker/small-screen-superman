36d80da073d7f026c4803d1a5a11140f3b29462e
Talia <talia.j.degisi@gmail.com>
2024-02-14 00:28:59 -0500

    Make 'Jump To' list Twig-based, not JS-based (and remove some old files)

--- tv/gallerysegment.php
+++
@@ -1,9 +0,0 @@
-<div id="galleryselection" class="galleryselection invisible">
-	<figure>
-		<img src="" alt="">
-		<figcaption></figcaption>
-	</figure>
-	<div class="close" onclick="closeGallerySelection(click);">X</div>
-	<div class="galleryscroll left" onclick="scrollThroughImages('left')"><img src="/superman-left.png"></div>
-	<div class="galleryscroll right" onclick="scrollThroughImages('right')"><img src="/superman-right.png"></div>
-</div>
