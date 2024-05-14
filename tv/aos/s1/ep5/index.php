db30f249069ad11d9de33069cfe7a9e0b11004f2
Talia <talia.j.degisi@gmail.com>
2024-05-13 22:24:43 -0400

    AOS s1ep5: Set up Gallery

--- tv/aos/s1/ep5/index.php
+++ tv/aos/s1/ep5/index.php
@@ -7,7 +7,13 @@ echo $twig->render('/tv-episode.html', [
   'overallEpNum' => 5,
   'epTitle' => 'The Monkey Mystery',
   'content' => file_get_contents('index-contents.html'),
-  'gallery' => array(),
+  'gallery' => array(
+    'scr012_Pepi_the_monkey_in_a_Superman_costume_playing_cymbals.jpg',
+    'scr013_Clark_Kent,_Lois_Lane,_and_a_crowd_of_kids_look_at_the_monkey_and_organ_grinder.jpg',
+    'scr034_Lois_is_attacked_by_a_man_with_a_gun.jpg',
+    'scr045_Superman_talks_with_Maria_as_she_wakes_up.jpg',
+    'scr072_Superman_knocks_the_heads_of_the_criminals,_Max_and_Crane,_together.jpg'
+  ),
   'imdbId' => 'tt0506627',
   'tacId' => 'MonkeyMysteryReview',
   'description' => 'Information, gallery, and more about The Haunted Lighthouse, the fourth episode of Adventures of Superman.'
