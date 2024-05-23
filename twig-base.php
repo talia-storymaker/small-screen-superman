19c2a7c18b2369657a09ffce95308ea924f26716
Talia <talia.j.degisi@gmail.com>
2024-05-23 00:07:53 -0400

    WIP: Prep work for scrolling between episodes
    Lots of very incomplete stuff here, including
    things that will be undone in the next commit.

--- twig-base.php
+++ twig-base.php
@@ -6,6 +6,18 @@ $twig->addFilter(
   new \Twig\TwigFilter('regexReplace', function ($string, $pattern, $replacement) {
   return preg_replace($pattern, $replacement, $string);
 }));
+$twig->addFilter(
+  new \Twig\TwigFilter('addZeroIfNeeded', function ($string) {
+  if (strlen($string) === 1) {
+    return "0" . $string;
+  } else if (strlen($string) === 2) {
+    return $string;
+  }
+}));
+$dirFunction = new \Twig\TwigFunction('getMagicDir', function () {
+  return __DIR__;
+});
+$twig->addFunction($dirFunction);
 $twig->addExtension(new \Twig\Extension\PhpFunctionExtension());
 $twig->addGlobal('currentDir', preg_replace('/[^\/]+$/', '', "https://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]"));
 ?>
