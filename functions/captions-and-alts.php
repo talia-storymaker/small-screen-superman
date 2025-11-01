5656df48eff243d830662612e623b7d555287627
talia-storymaker <talia.j.degisi@gmail.com>
2025-11-01 18:33:55 -0400

    Comment changes

--- functions/captions-and-alts.php
+++ functions/captions-and-alts.php
@@ -1,25 +1,29 @@
 <?php
 function captionsAndAlts() {
+  // move connection to a separate file later
   $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
   $dotenv->load();
-
-  // move connection to a separate file later
   $hostname   = $_ENV['DB_HOST'];
   $dbname     = $_ENV['DB_NAME'];
   $username   = $_ENV['DB_USER'];
   $password   = $_ENV['DB_PASS'];
+
+  /* Currently, captions and alts are always the same, and usually come from the
+  filename. But sometimes there's an override for the caption/alt in the database.
+  This function gets those when applicable. */
 
   try {
     $conn = new PDO("mysql:host=$hostname;dbname=$dbname", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   } catch(PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
+    return; 
+    // Exit for graceful failure, i.e. fall back to filename-based captions/alts.
+    // Still shows error at top - consider logging instead.
+    // Also slow. Fix later.
   }
 
   $gallery = glob("media/screencaps/thumbs/*.*");
-  /* Currently, captions and alts are always the same, and usually come from the
-  filename. But sometimes there's an override for the caption/alt in the database.
-  This gets those when applicable. */
   $stmt = $conn->prepare("SELECT caption FROM gallery WHERE filename = :filename");
 
   $overrides = [];
